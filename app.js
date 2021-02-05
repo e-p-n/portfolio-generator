eb0bfc8e5a7462817d0f7a9a486cfc68e46ed20a
Eric Normann <eric.n@me.com>
2021-02-05 17:29:02 -0500

    generate HTML from input

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer
@@ -136,19 +138,13 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err=> {
+            if (err) throw err;
+
+            console.log("Portfolio complete! Check out index.html to see the output!");
+        })
     });
-
-
-/*const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const pageHTML = generatePage(userName, github);
-
-
-fs.writeFile('./index.html', pageHTML, err=> {
-    if (err) throw err;
-
-    console.log("Portfolio complete! Check out index.html to see the output!");
-})*/
 
